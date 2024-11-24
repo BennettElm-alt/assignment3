cc248545eedabb1b1ee3e50684923d514842a9a2
Bennett Elmhurst <bennett.elmhurst@ontariotechu.net>
2024-11-23 22:49:23 -0500

    2

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/', require('./routes/assignments'));
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
 
-const mongoose = require('mongoose');
+
 
 mongoose.connect(process.env.DB_URI, { 
   useNewUrlParser: true, 
